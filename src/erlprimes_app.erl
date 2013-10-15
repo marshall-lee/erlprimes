26c0660a5527ac0a6b21bc6be0268c7492b3f7ec
marshall-lee <hashtable@yandex.ru>
2013-10-15 17:59:51 +0400

    move to src/ and create-app

---
+++ src/erlprimes_app.erl
@@ -0,0 +1,16 @@
+-module(erlprimes_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlprimes_sup:start_link().
+
+stop(_State) ->
+    ok.
